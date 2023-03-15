6658d313d6ad8cd97c8a6c0e3248eb325d5f9f80
Sonny3fingers <77343092+Sonny3fingers@users.noreply.github.com>
2023-03-15 12:25:43 +0100

    Added cleaner code to set booksByYearsData

--- src/App.js
+++ src/App.js
@@ -3,31 +3,28 @@ import "./App.css";
 import FormContainer from "./components/FormContainer";
 import TableContainer from "./components/table/TableContainer";
 import TotalBooksSection from "./components/TotalBooksSection";
-// import BoxModal from "./components/UI/BoxModal";
-// import EditBookModal from "./components/UI/EditBookModal";
 import SortTable from "./components/UI/SortTable";
 import EditContext from "./store/edit-context";
 import DeleteContext from "./store/delete-context";
 import BarChart from "./components/BarChart";
 import HeaderTableSection from "./components/UI/HeaderTableSection";
 import Header from "./components/Header";
+import BoxModal from "./components/UI/BoxModal";
 
 function App() {
   const [books, setBooks] = useState([]);
-  const [errorHandler, setErrorHandler] = useState();
-  const [chartData, setChartData] = useState({
-    labels: [],
-    datasets: [],
-  });
+  const [error, setError] = useState(null);
   const [sortByDate, setSortByDate] = useState(true);
   const editCtx = useContext(EditContext);
   const deleteCtx = useContext(DeleteContext);
-  const [readingBooksByYearsData, setReadingBooksByYearsData] = useState([]);
+  const [booksByYearsData, setBooksByYearsData] = useState([]);
+
   const fetchBooksHandler = useCallback(async () => {
-    const response = await fetch(
-      "https://book-project-168e4-default-rtdb.europe-west1.firebasedatabase.app/books.json"
-    );
     try {
+      const response = await fetch(
+        "https://book-project-168e4-default-rtdb.europe-west1.firebasedatabase.app/books.json"
+      );
+
       if (!response.ok) {
         throw new Error("Something went wrong.");
       }
@@ -51,7 +48,7 @@ function App() {
       editCtx.setItems(loadedBooks);
       deleteCtx.setItems(loadedBooks);
     } catch (error) {
-      setErrorHandler(error.message);
+      setError(error.message);
     }
   }, []);
 
@@ -76,59 +73,77 @@ function App() {
   }, [editCtx.items]);
 
   useEffect(() => {
-    if (books.length > 0) {
-      const readingYears = [];
-      books.forEach((book) => {
-        let readingYear = new Date(book.date).getFullYear();
-        if (!readingYears.includes(readingYear)) {
-          readingYears.push(readingYear);
-        }
-      });
+    if (isBooksList) {
+      const booksYears = getBooksYears();
 
-      const tempData = [];
-      readingYears.forEach((readingYear) => {
-        const data = [];
-        const year = readingYear;
-        for (let i = 0; i < 12; i++) {
-          const month = i < 9 ? "0" + (i + 1) : i + 1;
-          const readByMonth = books.filter(
-            (book) =>
-              new Date(book.date).getFullYear() === year &&
-              new Date(book.date).getMonth() === i
-          );
-          data.push(readByMonth.length);
-        }
-        tempData.push({
-          chartData: {
-            labels: [
-              "January",
-              "February",
-              "March",
-              "April",
-              "May",
-              "June",
-              "July",
-              "August",
-              "September",
-              "October",
-              "November",
-              "December",
-            ],
-            datasets: [
-              {
-                label: `Monthly read books by ${year}`,
-                data: data,
-                backgroundColor: "rgba(255, 206, 86, 0.2)",
-                borderWidth: 1,
-                borderColor: "rgba(255, 206, 86, 1)",
-              },
-            ],
-          },
-        });
-      });
-      setReadingBooksByYearsData(tempData);
+      const chartsBooksData = getChartData(booksYears);
+
+      setBooksByYearsData(chartsBooksData);
     }
   }, [books]);
+
+  const isBooksList = () => {
+    return books.length > 0;
+  };
+
+  const getBooksYears = () => {
+    const years = [];
+    books.forEach((book) => {
+      let year = new Date(book.date).getFullYear();
+      if (!years.includes(year)) {
+        years.push(year);
+      }
+    });
+    return years;
+  };
+
+  const getBooksAmountByMonth = (year) => {
+    const booksAmountByMonth = [];
+    for (let i = 0; i < 12; i++) {
+      const readByMonth = books.filter(
+        (book) =>
+          new Date(book.date).getFullYear() === year &&
+          new Date(book.date).getMonth() === i
+      );
+      booksAmountByMonth.push(readByMonth.length);
+    }
+    return booksAmountByMonth;
+  };
+
+  const getChartData = (booksYears) => {
+    const tempData = [];
+    for (let year of booksYears) {
+      let booksAmountByMonth = getBooksAmountByMonth(year);
+      tempData.push({
+        chartData: {
+          labels: [
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December",
+          ],
+          datasets: [
+            {
+              label: `Monthly read books by ${year}`,
+              data: booksAmountByMonth,
+              backgroundColor: "rgba(255, 206, 86, 0.2)",
+              borderWidth: 1,
+              borderColor: "rgba(255, 206, 86, 1)",
+            },
+          ],
+        },
+      });
+    }
+    return tempData;
+  };
 
   const addBookHandler = async (values) => {
     const response = await fetch(
@@ -150,7 +165,7 @@ function App() {
         onAddBook={addBookHandler}
         onFetchBooks={fetchBooksHandler}
       />
-      {errorHandler && <p>{errorHandler.message}</p>}
+      {error && <BoxModal title="Error" message={error} isNotValidate={true} />}
       <HeaderTableSection />
       <SortTable
         sortByDate={sortByDate}
@@ -159,9 +174,9 @@ function App() {
       />
       <TableContainer items={books} onFetchBooks={fetchBooksHandler} />
       <TotalBooksSection items={books} />
-      {!readingBooksByYearsData
+      {!booksByYearsData
         ? ""
-        : readingBooksByYearsData.map((item) => (
+        : booksByYearsData.map((item) => (
             <BarChart chartData={item.chartData} key={crypto.randomUUID()} />
           ))}
     </div>
